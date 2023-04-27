8ea5c96907850bf5f5e83014f9d5b1cdba6975a3
Sai Suvanth <saisuvanth@gmail.com>
2023-04-27 14:37:01 +0530

    hmmm

--- pagerank/utils.go
+++ pagerank/utils.go
@@ -20,13 +20,11 @@ func Combiner(ctx context.Context, inputs ...<-chan workerStruct) <-chan workerS
 		}
 	}
 
-	// add length of input channels to be consumed by mutiplexer
 	wg.Add(len(inputs))
 	for _, in := range inputs {
 		go multiplexer(in)
 	}
 
-	// close channel after all inputs channels are closed
 	go func() {
 		wg.Wait()
 		close(out)
